f480eef4b90bdca6a4462597a549557e60387e29
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-28 17:39:14 +0530

    Correction in email verification model

--- Backend/models/EmailVerification.js
+++ Backend/models/EmailVerification.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const emailVerificationSchema = new mongoose.Schema({
     userId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'user',
         required: true
     },
     otp:{

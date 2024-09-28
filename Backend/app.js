b8a7d80d6758f0fc8f639e3b66c209d7591580b5
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-28 12:22:15 +0530

    configured cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,21 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-const app = express()
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+const corsOption = {
+    origin: process.env.FRONTED_HOST ,
+    Credential: true,
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOption));
+
+app.use(express.json());
+
 
 app.listen(PORT, ()=>{
     console.log(`Serevr listening at http://localhost:${PORT}`);
-})
+});

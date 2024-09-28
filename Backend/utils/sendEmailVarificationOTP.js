331df9d03d444cd742bd376d2443dfd9043fea23
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-28 17:18:46 +0530

    send email varification otp setup

---
+++ Backend/utils/sendEmailVarificationOTP.js
@@ -0,0 +1,22 @@
+import transporter from "../config/emailConfig";
+
+
+const sendEmailVarificationOTP = async (req, user) => {
+    // Generate random 4-digit number
+    const otp = Math.floor(1000 + Math.random()*9000);
+
+    // OTP varification Link
+    const otpVerificationLink = `${process.env.FRONTED_HOST}/account/verifi-email`;
+
+    await transporter.sendMail({
+        form: process.env.EMAIL_FROM,
+        to: user.email,
+        subject: 'OTP - Varify your account',
+        html: `<p>Dear ${user.name},</p><p>Thank you for signing up with our website. To complete your registration, please verify your email address by entering the following one-time password (OTP):${otpVerificationLink} </p>
+        <h2>OTP: ${otp}</h2>
+        <p>This OTP is valid for 15 minutes. If you didn't request this OTP, please ignore this email.</p>`
+    })
+    return otp
+}
+
+export default sendEmailVarificationOTP

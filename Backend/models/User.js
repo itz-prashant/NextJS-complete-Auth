6df6d2cd5c7ff77ca84ae4f94342ec4f951f0c04
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-28 13:02:40 +0530

    User model  created

---
+++ Backend/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim:true,
+        unique: true,
+        lowercae: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    isVarified:{
+        type: Boolean,
+        default: false
+    },
+    roles:{
+        type: [String],
+        enum:["user", "admin"],
+        default: "user"
+    }
+})
+
+const UserModel = mongoose.model('user', userSchema);
+
+export default UserModel;
